c2a76903b3943eed5a2bcea149d94f19651a0b0d
Satwinder Singh <satwinder4955@gmail.com>
2024-03-12 16:02:49 -0400

    changes

--- config/globals.js
+++ config/globals.js
@@ -1,5 +1,22 @@
-const configurations = {
+const mongoose = require('mongoose');
+
+
+const mongoURI = {
   'db': 'mongodb+srv://user4512:comp2068@cluster0.fgk9ofi.mongodb.net/projectTrackerApplication'
 }
 
-module.exports = configurations;
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+ 
+  
+});
+
+const db = mongoose.connection;
+// Event handlers for MongoDB connection
+db.on('error', console.error.bind(console, 'MongoDB Atlas connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB Atlas successfully');
+});
+require('../models/project');
+// Export the database connection
+module.exports = db;
